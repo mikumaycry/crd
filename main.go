d1794d59fd9eaa252308f04e19d2da6d669b5ad0
mikumaycry <me@wbuntu.com>
2022-03-03 18:17:34 +0800

    commit

--- main.go
+++ main.go
@@ -85,10 +85,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Bundle")
 		os.Exit(1)
 	}
-	if err = (&webv1.Bundle{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Bundle")
-		os.Exit(1)
-	}
+	/*
+		if err = (&webv1.Bundle{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Bundle")
+			os.Exit(1)
+		}
+	*/
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
